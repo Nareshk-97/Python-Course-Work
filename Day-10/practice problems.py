46f4419cb7e20ac6437619f75639648a03a9d47f
Naresh <144024072+Nareshk-97@users.noreply.github.com>
2026-06-08 12:40:55 +0530

    Add files via upload

---
+++ Day-10/practice problems.py
@@ -0,0 +1,132 @@
+'''a=int(input())
+if a<0:
+    print("negative number")
+else:
+    print("positive number")'''
+
+
+'''a=int(input())
+if a%2==0:
+    print("even")
+else:
+    print("odd")'''
+
+
+
+'''a=int(input())
+if a%5==0:
+    print("divisible by 5")
+else:
+    print("not divisible by 5")'''
+
+
+'''a=int(input())
+if a%3==0 and a%7==0:
+    print("divisible by 3 and 7")
+else:
+     print("not divisible by 3 and 7")'''
+
+
+'''a=int(input())
+if a%400==0:
+    print("leap year")
+else:
+    print("not leep year")'''
+
+
+'''a=int(input())
+if a%400==0 or a%4==0 and a%100!=0:
+    print("leap year")
+else:
+    print("not leep year")'''
+
+
+'''marks=int(input())
+if marks>=35:
+    print("pass")
+else:
+    pront("Fail")'''
+
+
+'''a=int(input())
+if a>=100 and a<=999:
+    print("3 digit number")
+else:
+    print("not 3 digit number")'''
+
+
+
+'''a=input()
+if a in "aeiouAEIOU":
+    print("vowel")
+else:
+    print("not vowel")'''
+
+
+
+'''a=int(input())
+b=int(input())
+if a>b:
+    print("Greatest is",a)
+else:
+    print("Greatest is",b)'''
+
+
+'''a=int(input())
+b=int(input())
+if a<b:
+    print("smallest is ",a)
+else:
+    print("smallest is ",b)'''
+
+
+'''a=int(input())
+if a==0:
+    print("number is Zero")
+else:
+    print("number if not Zero")'''
+
+
+
+'''a=int(input())
+if a%10==0:
+    print("multiple by 10")
+else:
+    print("not multiple by 10")'''
+
+
+
+'''a=int(input())
+if a>=18:
+    print("Eligible to vote")
+else:
+    print("Not Eligible to vote")'''
+
+
+'''a=int(input())
+if a>=1 and a<=100:
+    print("in range")
+else:
+    print("not in range")'''
+
+
+
+'''a=int(input())
+b=int(input())
+if b**2==a:
+    print(f"{a}is square of {b}")
+else:
+    print(f"{a} is not square of {b}")'''
+
+
+
+'''a=input()
+b=input()
+if a==b:
+    print("Two strings are equal")
+else:
+    print("Two strings are not equal")'''
+
+    
+
+
