9570a82b97c5fdbb9b21c58a2975394041f093cd
Naresh <144024072+Nareshk-97@users.noreply.github.com>
2026-06-29 11:44:06 +0530

    Add files via upload

---
+++ Day-1/kw.py
@@ -0,0 +1,36 @@
+import keyword
+print(keyword.kwlist)          # Prints all keywords
+print(len(keyword.kwlist))     # Prints total count
+Myvar=10
+Myvar1=10
+#Myvar@1=10
+#SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?
+my_var=10
+my var=10
+#SyntaxError: invalid syntax
+myvar=10
+Myvar=10
+
+_myvar=10
+if = 10
+#SyntaxError: invalid syntax
+var=10
+var=20
+
+
+var=20
+
+
+a=b=c=10
+a
+b
+
+c
+
+a,b,c=10,20,30
+a
+
+b
+
+c
+
